20da4b70bb677b5880577cfcb8ff7bd2756e362d
Subroto89 <me.subrotosarker@gmail.com>
2025-09-19 02:24:49 +0600

    Added: Hearder Part

--- src/components/HomePage/FarmerSuccessStoriesSection.jsx
+++ src/components/HomePage/FarmerSuccessStoriesSection.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const FarmerSuccessStoriesSection = () => {
   return (
-    <div>
+    <div className="py-16 rounded mt-16">
         {/* Header Part -------------------------------- */}
-      <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+      <div className="text-center mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
         Farmer Success Stories
       </h2>
 
@@ -12,6 +13,7 @@ const FarmerSuccessStoriesSection = () => {
           Real farmers, real results. Discover how Root Farming is transforming
           agricultural businesses across the country.
         </p>
+      </div>
     </div>
   );
 };
