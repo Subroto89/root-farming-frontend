92732afda54e704d0bc245711e3f89edba906e72
Dhyman Choudhury <dhyman2029@gmail.com>
2025-09-19 02:41:42 +0600

    added base HeroSection structure with left and right placeholders

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+// src/components/HeroSection.jsx
+import React from "react";
 
 const HeroSection = () => {
-    return (
-        <>
-            This is Hero Section
-        </>
-    );
+  return (
+    <section className="bg-white py-16 px-5 md:px-10 lg:px-16">
+      <div className="mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+        {/* Left Content */}
+        <div className="space-y-6">
+          
+        </div>
+
+        {/* Right Image */}
+        <div className="flex justify-center md:justify-end">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
