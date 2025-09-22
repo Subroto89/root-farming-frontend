be91aadbe8618f988d35c8521968f396788a1474
Saiful Islam <sumon.sis169@gmail.com>
2025-09-22 23:37:14 +0600

    dashboard layout page created

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,42 +1,44 @@
-import { createBrowserRouter } from "react-router";
-import RootLayout from "../layouts/RootLayout";
-import Home from "../pages/HomePage/Home";
-import AboutUs from "../pages/AboutUsPage/AboutUs";
-import ContactUs from "../pages/ContactUsPage/ContactUs";
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../layouts/RootLayout';
+import Home from '../pages/HomePage/Home';
+import AboutUs from '../pages/AboutUsPage/AboutUs';
+import ContactUs from '../pages/ContactUsPage/ContactUs';
 
 const router = createBrowserRouter([
-   // -------------------------------------------
-   // Root Layout
-   // -------------------------------------------
-   {
-      path: "/",
-      Component: RootLayout,
-      children: [
-         {
-            path: "/",
-            index: true,
-            Component: Home,
-         },
-         {
-            path: "/about",
-            Component: AboutUs,
-         },
-         {
-            path: "/contact",
-            Component: ContactUs,
-         },
-      ],
-   },
+  // -------------------------------------------
+  // Root Layout
+  // -------------------------------------------
+  {
+    path: '/',
+    Component: RootLayout,
+    children: [
+      {
+        path: '/',
+        index: true,
+        Component: Home,
+      },
+      {
+        path: '/about',
+        Component: AboutUs,
+      },
+      {
+        path: '/contact',
+        Component: ContactUs,
+      },
+    ],
+  },
 
-   // -------------------------------------------
-   // Dashboard Layout
-   // -------------------------------------------
-   {},
+  // -------------------------------------------
+  // Dashboard Layout
+  // -------------------------------------------
+  {
+    path: '/',
+  },
 
-   // -------------------------------------------
-   // Auth Layout
-   // -------------------------------------------
-   {},
+  // -------------------------------------------
+  // Auth Layout
+  // -------------------------------------------
+  {},
 ]);
 
 export default router;
