f7817adc31e6350aee83aa60088ccceb91f73fe3
Saiful Islam <sumon.sis169@gmail.com>
2025-09-24 00:07:47 +0600

    change coded

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router";
-import router from "./routes/router";
-import "leaflet/dist/leaflet.css";
-import "leaflet/dist/leaflet.css";
-import "leaflet-geosearch/dist/geosearch.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router';
+import router from './routes/router';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/dist/geosearch.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // 1. Create a client instance
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
