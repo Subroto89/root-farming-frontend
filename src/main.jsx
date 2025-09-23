5a6180c7f43763f6482958541b0bd935d01f1ee3
Bapan Modak <bapanm2317@gmail.com>
2025-09-23 13:59:54 +0530

    installed React Leaflet.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/router";
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
