9cbe0249dcb54a7797feaa50ea0826a384e3ab93
Dhyman Choudhury <dhyman2029@gmail.com>
2025-10-07 03:09:46 +0600

    added specialists salary page

--- src/components/Dashboard/Menu/AdminMenu/FinancialAndCommissions/AgriSalaryManagement/SalaryDetails.jsx
+++ src/components/Dashboard/Menu/AdminMenu/FinancialAndCommissions/AgriSalaryManagement/SalaryDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import InputField from "../../../../components/shared/InputField/InputField";
+import InputField from "../../../../../shared/InputField/InputField";
 
 export default function SalaryDetails({ specialist}) {
   const handleSubmit = (e) => {
