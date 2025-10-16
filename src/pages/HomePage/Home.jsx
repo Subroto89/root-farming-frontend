89fe1cb5e60834590896cfdf524c1fee39de870e
Saiful Islam <sumon.sis169@gmail.com>
2025-10-17 02:35:06 +0600

    customer lateste review section done

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import HeroSection from '../../components/HomePage/HeroSection';
 import WeatherForecastSection from '../../components/HomePage/WeatherForecastSection';
-import OurProductsSection from '../../components/HomePage/OurProductsSection';
-import HowItWorksSection from '../../components/HomePage/HowItWorksSection';
-import OurGrowingImpactSection from '../../components/HomePage/OurGrowingImpactSection';
 import FarmerSuccessStoriesSection from '../../components/HomePage/FarmerSuccessStoriesSection';
 import OurTrustedPartnersSection from '../../components/HomePage/OurTrustedPartnersSection';
 import BecomePartner from '../../components/HomePage/BecomePartner';
 import Features from '../../components/HomePage/Features';
 import HowItWorks from '../../components/HomePage/HowItWorks';
 import AdvertisementSection from '../../components/HomePage/AdvertisementSection';
+import LatestReviews from '../../components/HomePage/LatestReviews';
 
 const Home = () => {
   return (
@@ -20,8 +18,8 @@ const Home = () => {
       <Features />
       <HowItWorks />
       <AdvertisementSection />
+      <LatestReviews />
 
-      <OurGrowingImpactSection />
       <FarmerSuccessStoriesSection />
       <OurTrustedPartnersSection />
       <BecomePartner />
