53e78242b1359ae9f760bf86cdfb0c9d44162f13
Saiful Islam <sumon.sis169@gmail.com>
2025-10-17 03:02:44 +0600

    wishlisht fachit data api crated

---
+++ src/allProductsApi/UseWishlist.jsx
@@ -0,0 +1,46 @@
+import useAxiosSecure from '@/hooks/useAxiosSecure';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useWishlist = userEmail => {
+  const axiosSecure = useAxiosSecure();
+  return useQuery({
+    queryKey: ['wishlist', userEmail],
+    enabled: !!userEmail,
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/wishlist/${userEmail}`);
+      return data;
+    },
+  });
+};
+
+export const useAddToWishlist = () => {
+  const axiosSecure = useAxiosSecure();
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: async item => {
+      const { data } = await axiosSecure.post('/wishlist', item);
+      return data;
+    },
+    onSuccess: (_, variables) => {
+      if (variables.userEmail)
+        qc.invalidateQueries(['wishlist', variables.userEmail]);
+    },
+  });
+};
+
+export const useDeleteWishlist = () => {
+  const axiosSecure = useAxiosSecure();
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ userEmail, productId }) => {
+      const { data } = await axiosSecure.delete(
+        `/wishlist/${userEmail}/${productId}`
+      );
+      return data;
+    },
+    onSuccess: (_, variables) => {
+      qc.invalidateQueries(['wishlist', variables.userEmail]);
+    },
+  });
+};
